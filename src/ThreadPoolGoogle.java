538e0e7e9f30cc99e91baf9b37fd94eee8da2d85
mynameisi <test@nowhere.com>
2013-08-21 22:35:34 +0800

    https://github.com/mynameisi/demo_java_shutDownHook.git

--- src/ThreadPoolGoogle.java
+++ src/ThreadPoolGoogle.java
@@ -6,7 +6,9 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ThreadPoolExtended {
+import com.google.common.util.concurrent.MoreExecutors;
+
+public class ThreadPoolGoogle {
 	private static Logger logger = LoggerFactory.getLogger(Task.class);
 
 	public static void main(String[] args) {
@@ -14,19 +16,21 @@ public class ThreadPoolExtended {
 		final ExecutorService tPool = Executors.newFixedThreadPool(3);
 
 		// //////////////////////2. 注册关闭线程池///////////////////
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				logger.debug("程序结束，线程池开始关闭");
-
-				// 注意: 线程池的"安详"的关闭，要以下两个步骤
-				tPool.shutdown();// 停止接受新任务，但是老任务不停止
-				try {
-					tPool.awaitTermination(1, TimeUnit.MINUTES);
-				} catch (InterruptedException e) {
-				}
-			}
-		});
+		// Runtime.getRuntime().addShutdownHook(new Thread() {
+		// @Override
+		// public void run() {
+		// logger.debug("程序结束，线程池开始关闭");
+		//
+		// // 注意: 线程池的"安详"的关闭，要以下两个步骤
+		// tPool.shutdown();// 停止接受新任务，但是老任务不停止
+		// try {
+		// tPool.awaitTermination(1, TimeUnit.MINUTES);
+		// } catch (InterruptedException e) {
+		// }
+		// }
+		// });
+		// 以上所有注释掉的代码，等价于下面这一句话
+		MoreExecutors.addDelayedShutdownHook(tPool, 1, TimeUnit.MINUTES);
 		// ///////////////////////////////////////////////////
 
 		// 3. 把10个任务发给着3个worker去做
@@ -41,18 +45,5 @@ public class ThreadPoolExtended {
 				System.exit(0);
 			}
 		}
-	}
-}
-
-class Task implements Runnable {
-	private static Logger logger = LoggerFactory.getLogger(Task.class);
-	private int id;
-
-	public Task(int id) {
-		this.id = id;
-	}
-
-	public void run() {
-		logger.debug("线程:" + Thread.currentThread().getName() + " 在处理任务:" + id);
 	}
 }
